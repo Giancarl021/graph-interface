bc1941e796554cfccf136e75c26a8df65ec7352c
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-05-30 10:41:33 -0300

    Divided functions in hash.js

--- util/hash.js
+++ util/hash.js
@@ -2,5 +2,11 @@ const { createHash } = require('crypto');
 
 module.exports = function(credentials) {
     const { tenantId, clientId, clientSecret } = credentials;
-    return createHash('sha256').update(tenantId + clientId + clientSecret).digest('hex');
+    return hash(tenantId + clientId + clientSecret);
+}
+
+function hash(string) {
+    return createHash('sha256')
+        .update(string)
+        .digest('hex');
 }
