31d9fa0380beabee7776f85da05a9ea2f57c6770
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-06-09 11:22:44 -0300

    Added basic functions

--- util/redis-cache.js
+++ util/redis-cache.js
@@ -3,5 +3,16 @@ const redis = require('redis');
 module.exports = function(clientOptions) {
     const client = redis.createClient(clientOptions);
 
-    
+    async function set() {
+
+    }
+
+    async function get() {
+
+    }
+
+    return {
+        set,
+        get
+    }
 }
