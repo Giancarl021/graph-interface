2e3fe92abfc17a4b40cc09381bd971c89908feaf
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-08-10 17:15:15 -0300

    Added key "as" in fields property

--- util/object.js
+++ util/object.js
@@ -12,6 +12,16 @@ module.exports = function (source) {
         for (const field of fields) {
             if (source.hasOwnProperty(field)) {
                 r[field] = source[field];
+            } else if(field.includes(' as ')) {
+                const [_prop, _as] = field.split(' as ');
+
+                if(!_as) throw new Error('Field new name needs to be valid!');
+
+                if(!source.hasOwnProperty(_prop)) {
+                    if(strict) throw new Error('Source object does not have field ' + field);
+                } else {
+                    r[_as] = source[_prop];
+                }
             } else if (strict) {
                 throw new Error('Source object does not have field ' + field);
             }
